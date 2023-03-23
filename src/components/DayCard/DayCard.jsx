e49476d7689f9d3b21e11710bdb512fe7906913c
Gurkirat-s <gurkiratsingh@hotmail.ca>
2023-03-23 16:02:51 -0400

    Added 3 day weather forecast

---
+++ src/components/DayCard/DayCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './DayCard.css';
+import moment from 'moment';
+
+const DayCard = ({ date, temp, min, max, icon, condition }) => {
+  //Convert to EST from GMT
+  const day = moment((date + 14400) * 1000).format('dddd');
+
+  return (
+    <div className="day-card">
+      <h3>{day}</h3>
+      <div className="weather">
+        <div className="condition">
+          <img src={icon} alt="Condition icon" />
+          <p>{condition}</p>
+        </div>
+        <div className="right-weather">
+          <h4>
+            {temp} <span>&deg;C</span>
+          </h4>
+          <div class="min-max">
+            <p>Min: {min} &deg;C</p>
+            <p>Max: {max} &deg;C</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DayCard;
--- src/components/WeatherPage/WeatherPage.jsx
+++ src/components/WeatherPage/WeatherPage.jsx
@@ -4,6 +4,7 @@ import './WeatherPage.css';
 
 import WeatherCard from '../WeatherCard/WeatherCard';
 import HourlyCard from '../HourlyCard/HourlyCard';
+import DayCard from '../DayCard/DayCard';
 import { ThreeDots } from 'react-loader-spinner';
 
 export const WeatherPage = ({ location }) => {
@@ -15,6 +16,7 @@ export const WeatherPage = ({ location }) => {
     pressure_mb: 0,
   });
   const [forecast, setForecast] = useState([]);
+  const [forecast3Days, setForecast3Days] = useState([]);
   const [locationInfo, setLocationInfo] = useState({
     name: '',
     region: '',
@@ -32,13 +34,17 @@ export const WeatherPage = ({ location }) => {
 
       const forecastData = await getForecast(location);
 
-      console.log(forecastData.current);
+      // console.log(forecastData);
 
       setWeather(forecastData.current);
       setCondition(forecastData.current.condition);
       setLocationInfo(forecastData.location);
 
       setForecast(get24HoursForecast(forecastData.forecast.forecastday));
+      setForecast3Days(forecastData.forecast.forecastday);
+
+      console.log(forecastData.forecast.forecastday.length);
+      console.log(forecastData.forecast.forecastday);
 
       setLoading(false);
     };
@@ -84,14 +90,33 @@ export const WeatherPage = ({ location }) => {
             condition={condition}
             location={locationInfo}
           />
-          <div className="hourly-grid">
-            {forecast.map((item, index) => {
+          <div className="hourly-container">
+            <h2>24 Hour Forecast</h2>
+            <div className="hourly-grid">
+              {forecast.map((item, index) => {
+                return (
+                  <HourlyCard
+                    key={index}
+                    temp={item.temp_c}
+                    icon={item.condition.icon}
+                    weatherTime={item.time}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <div className="daily-container">
+            {forecast3Days.map((item, index) => {
               return (
-                <HourlyCard
+                <DayCard
                   key={index}
-                  temp={item.temp_c}
-                  icon={item.condition.icon}
-                  weatherTime={item.time}
+                  date={item.date_epoch}
+                  temp={item.day.avgtemp_c}
+                  min={item.day.mintemp_c}
+                  max={item.day.maxtemp_c}
+                  icon={item.day.condition.icon}
+                  condition={item.day.condition.text}
+                  date={item.date_epoch}
                 />
               );
             })}
